11f14763b015e73df170c13391af2e501d6f74b8
Mohammad <moeq24@yahoo.com>
2021-05-27 21:06:55 -0500

    Added New class

---
+++ src/day47_constructors/StudentObjects.java
@@ -0,0 +1,13 @@
+package day47_constructors;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+
+        Student st3 = new Student("Mohammad");
+        Student st4 = new Student("Idris");
+
+        Student st5 = new Student("Jinan" , 33);
+    }
+}
